4d73cf780682571a7e652c220ab5ece717c53651
Mo'men Abu Gharbieh <momen6022@gmail.com>
2023-05-28 13:50:33 +0300

    Register & Contact Us page

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React, { useState, useRef } from "react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import DatePicker from "react-native-datepicker";
+import ImagePicker from "react-native-image-picker";
+import { CheckBox } from 'react-native-elements';
+
 import {
   StyleSheet,
   Text,
@@ -10,6 +14,8 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   TextInput,
+  Button,
+  Check,
 } from "react-native";
 
 // Home Container Component
@@ -26,6 +32,73 @@ const HomeContainer = ({ handleGoBack }) => {
 
 // Register Page Component
 const RegisterPage = ({ handleGoBack }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const options = ["Option 1", "Option 2", "Option 3"];
+  const [mail, setMail] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedDegree, setSelectedDegree] = useState("");
+  const [major, setMajor] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [position, setPosition] = useState("");
+  const [selectedGender, setSelectedGender] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  // Function to handle image selection
+  const selectImage = () => {
+    ImagePicker.showImagePicker(
+      {
+        title: "Select Picture",
+        mediaType: "photo",
+        maxWidth: 500,
+        maxHeight: 500,
+      },
+      (response) => {
+        if (!response.didCancel && !response.error) {
+          setSelectedImage(response.uri);
+        }
+      }
+    );
+  };
+
+  const [checked, setChecked] = useState(false);
+
+  const toggleCheckbox = () => {
+    setChecked(!checked);
+  };
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setDropdownOpen(false);
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleSend = () => {
+    // Handle send functionality here
+    console.log("firstName:", firstName);
+    console.log("lastName:", lastName);
+    console.log("date:", selectedDate);
+    console.log("Mail:", mail);
+    console.log("Selected City:", selectedCity);
+    console.log("Selected Degree:", selectedDegree);
+    console.log("major:", major);
+    console.log("company:", companyName);
+    console.log("position:", position);
+    console.log("Selected Gender:", selectedGender);
+    console.log("phoneNumber:", phoneNumber);
+  };
+
   return (
     <ScrollView style={styles.pageContainer}>
       <View style={styles.pageHeader}>
@@ -37,7 +110,155 @@ const RegisterPage = ({ handleGoBack }) => {
         </TouchableOpacity>
       </View>
       <View style={styles.pageContentContainer}>
-        <Text style={styles.pageText}>This is the Register page.</Text>
+        <Text style={styles.label}> First Name </Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={firstName}
+          onChangeText={setFirstName}
+        />
+        <Text style={styles.label}> Last Name</Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={lastName}
+          onChangeText={setLastName}
+        />
+        <View style={styles.container}>
+          <DatePicker
+            style={styles.datePicker}
+            date={selectedDate}
+            mode="date"
+            placeholder="Select date"
+            format="YYYY-MM-DD"
+            minDate="1900-01-01"
+            maxDate="2100-12-31"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                display: "none",
+              },
+              dateInput: {
+                borderWidth: 0,
+              },
+            }}
+            onDateChange={handleDateChange}
+          />
+        </View>
+        <Text style={styles.label}> Mail</Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={mail}
+          onChangeText={setMail}
+        />
+        <Text style={styles.label}>Select City</Text>
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity style={styles.dropdown} onPress={toggleDropdown}>
+            <Text style={styles.dropdownText}>{selectedCity || "Select"}</Text>
+          </TouchableOpacity>
+          {isDropdownOpen && (
+            <View style={styles.optionsContainer}>
+              {options.map((option) => (
+                <TouchableOpacity
+                  key={option}
+                  style={styles.option}
+                  onPress={() => handleOptionSelect(option)}
+                >
+                  <Text style={styles.optionText}>{option}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
+        </View>
+        <Text style={styles.label}>Select Degree</Text>
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity style={styles.dropdown} onPress={toggleDropdown}>
+            <Text style={styles.dropdownText}>
+              {selectedDegree || "Select"}
+            </Text>
+          </TouchableOpacity>
+          {isDropdownOpen && (
+            <View style={styles.optionsContainer}>
+              {options.map((option) => (
+                <TouchableOpacity
+                  key={option}
+                  style={styles.option}
+                  onPress={() => handleOptionSelect(option)}
+                >
+                  <Text style={styles.optionText}>{option}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
+        </View>
+        <Text style={styles.label}> Major </Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={mail}
+          onChangeText={setMajor}
+        />
+        <Text style={styles.label}> Company Name </Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={companyName}
+          onChangeText={setCompanyName}
+        />
+        <Text style={styles.label}> Position </Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={position}
+          onChangeText={setPosition}
+        />
+        <Text style={styles.label}>Select Gender</Text>
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity style={styles.dropdown} onPress={toggleDropdown}>
+            <Text style={styles.dropdownText}>
+              {selectedGender || "Select"}
+            </Text>
+          </TouchableOpacity>
+          {isDropdownOpen && (
+            <View style={styles.optionsContainer}>
+              {options.map((option) => (
+                <TouchableOpacity
+                  key={option}
+                  style={styles.option}
+                  onPress={() => handleOptionSelect(option)}
+                >
+                  <Text style={styles.optionText}>{option}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          )}
+        </View>
+        <Text style={styles.label}> Phone Number </Text>
+        <TextInput
+          // placeholder="Enter Your Name"
+          style={styles.input}
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
+        />
+        <View>
+          <Button title="Upload Picture" onPress={selectImage} />
+          {selectedImage && (
+            <Image
+              source={{ uri: selectedImage }}
+              style={{ width: 200, height: 200 }}
+            />
+          )}
+        </View>
+        <CheckBox
+        title='I confirm that the information I provided is reliable and accurate, and I also agree to bear the responsibility for publishing unreliable information, as well as I agree to publish the image of my personal file, which does not have any copying rights, publicly on the site and on the application the Arab Academic Forum database.'
+        checked={checked}
+        onPress={toggleCheckbox}
+      />        
+        <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -87,7 +308,7 @@ const ContactPage = ({ handleGoBack }) => {
   const [mail, setMail] = useState("");
   const [selectedSubject, setSelectedOption] = useState("");
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const options = ['Option 1', 'Option 2', 'Option 3'];
+  const options = ["Option 1", "Option 2", "Option 3"];
   const [message, setMessage] = useState("");
 
   const toggleDropdown = () => {
@@ -134,7 +355,7 @@ const ContactPage = ({ handleGoBack }) => {
         />
         <Text style={styles.label}>Subject</Text>
         <View style={styles.dropdownContainer}>
-          <TouchableOpacity style={styles.dropdown} onPress={toggleDropdown}>            
+          <TouchableOpacity style={styles.dropdown} onPress={toggleDropdown}>
             <Text style={styles.dropdownText}>
               {selectedSubject || "Select"}
             </Text>
@@ -452,5 +673,8 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     color: "white",
+  },
+  datePicker: {
+    width: 200,
   },
 });
