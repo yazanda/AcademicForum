8e241d539cc9213e7e5cd828b34f13f1971d72a0
Yazan <yazandoifi@gmail.com>
2023-06-07 21:35:03 +0300

    podcast page (liked to data base, design done, translation done)

--- src/screens/PodcastScreen.js
+++ src/screens/PodcastScreen.js
@@ -1,12 +1,41 @@
-import React, {useRef, useState} from "react"
-import Header from "../components/Header";
-import {Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View,} from "react-native"
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, StyleSheet, View, ScrollView, Text, Image, FlatList, TouchableOpacity, I18nManager } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { Video } from 'expo-av';
+import axios from 'axios';
+import Header from '../components/Header';
 
 export default function PodcastScreen({navigation}) {
-    const handleGoBack = () => {
-        // setSidebarOpen(false);
-        navigation.navigate("StartScreen");
+    const [videos, setVideos] = useState([]);
+    const {t, i18n} = useTranslation();
+    const isRTL = I18nManager.isRTL;
+
+    useEffect(() => {
+        fetchVideos();
+    }, []);
+
+    const fetchVideos = async () => {
+        try {
+          const response = await axios.get('https://almuntada.onrender.com/api/v1/podcasts/isActive/true');
+          setVideos(response.data);
+        } catch (error) {
+          console.error(error);
+        }
     };
+
+    const renderItem = ({ item }) => (
+        <View style={styles.videoContainer}>
+          <Video
+            source={{ uri: item.podcastUrl }}
+            style={styles.video}
+            shouldPlay
+            resizeMode="cover"
+            useNativeControls 
+          />
+          <Text style={styles.videoTitle}>{item.title}</Text>
+        </View>
+    );
+
 
     return (
         <SafeAreaView style={styles.container}>
@@ -14,17 +43,23 @@ export default function PodcastScreen({navigation}) {
                 style={styles.header}
                 navigation={navigation}
             />
-            <View style={styles.pageContentContainer}>
-                <TouchableOpacity onPress={()=>{handleGoBack()}}>
+            <ScrollView style={styles.pageContentContainer}>
+                <TouchableOpacity onPress={()=> navigation.navigate("StartScreen")}>
                     <Image
                         source={require("../../assets/FinalLogo.png")}
                         style={styles.logo}
                     />
                 </TouchableOpacity>
-                <View style={styles.pageContentContainer}>
-                    <Text style={styles.pageText}>This is the Podcast page.</Text>
+                <View style={styles.pageContent}>
+                    <Text style={[styles.firstText, isRTL && styles.rtlText]}>{t('podcastpage.desc')}</Text>
+                    <Text style={[styles.secondText, isRTL && styles.rtlText]}>{t('podcastpage.subdesc')}</Text>
+                    <FlatList
+                      data={videos}
+                      renderItem={renderItem}
+                      keyExtractor={(item) => item.id.toString()}
+                    />
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 
@@ -45,14 +80,42 @@ const styles = StyleSheet.create({
         width: 200,
         height: 75,
         resizeMode: "contain",
+        alignSelf: 'center',
     },
     pageContentContainer: {
-        flex: 1,
-        alignItems: "center",
+        flexGrow: 1,
     },
-    pageText: {
-        fontSize: 20,
-        fontWeight: "bold",
+    pageContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+    },
+    firstText: {
         color: "black",
+        textAlign: 'center',
+    },
+    secondText: {
+        color: "black",
+        paddingTop: 10,
+        textAlign: 'center',
+    },
+    videoContainer: {
+        paddingTop: 30,
+        alignItems: "center",
+        width: '100%',
+        marginBottom: 16,
+    },
+    video: {
+      width: 365,
+      height: 200,
+    },
+    videoTitle: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginTop: 8,
+    },
+    rtlText: {
+        textAlign: 'right',
     },
 });
