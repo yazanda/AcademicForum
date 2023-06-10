f3ae8d63cbbd2533eb741bfe313592dd05242e5f
zenabob <zenabobedat2001@gmail.com>
2023-06-10 15:00:10 +0300

    update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -17,6 +17,7 @@ import Dropdown from "../components/DropDown";
 import Modal from '../components/Modal'
 import { getDegreeList } from '../lists/degree';
 import { getCityList } from '../lists/list';
+import End from '../components/End';
 
 const window = Dimensions.get('window');
 
@@ -24,29 +25,7 @@ const window = Dimensions.get('window');
 export default function SearchScreen({navigation}) {
     const {t, i18n} = useTranslation();
     const [modalVisible, setModalVisible] = useState(false);
-    const handleOpenPDF = async () => {
-        try {
-         
-          const localUri = require('../../assets/terms.pdf');
-    
-       
-          navigation.navigate('PDFScreen', { uri: localUri });
-        } catch (error) {
-          console.log('Error opening PDF:', error);
-        }
-      };
-    const openInstagram = () => {
-        const instagramURL = 'https://www.instagram.com/almuntada_/?fbclid=IwAR1SIvAHoEaeXCxT1pDt1mzbfSL_8A7tOeBpB-GSRur81TELRu28gdgtG5I';
-        Linking.openURL(instagramURL);
-      };
-      const openLinkedin = () => {
-        const linkedinURL = 'https://www.linkedin.com/company/almuntada';
-        Linking.openURL(linkedinURL);
-      };
-      const openFacebook = () => {
-        const facebookURL = 'https://www.facebook.com/Almuntada.ac';
-        Linking.openURL(facebookURL);
-      };
+
     const [city, setCity] = useState('');
     const [degree, setDegree] = useState('');
     const toggleModal = () => {
@@ -107,65 +86,7 @@ export default function SearchScreen({navigation}) {
                         </TouchableOpacity>
                     </View>
                 </View>
-                <Modal modalVisible={modalVisible} toggleModal={toggleModal}/>
-                    <View style={{height: 70}}/>
-                    <View style={styles.rectangular}>
-                    <View style={styles.halfReg}>
-                    <View style={{height: 20}}/>
-                    <Text style={styles.TextInfo}>{t('footer.contactus')}</Text>
-                    <View style={{height: 20}}/>
-                    <View style={styles.info}> 
-                    <FontAwesome name="phone" style={{fontSize:20,color: '#05063F' }} />
-                     <View style={{width: 10}}/> 
-                    <Text style={styles.nfo}>052-9086918</Text>
-                        </View>
-                        <View style={{height: 10}}/>
-                        <View style={styles.info}> 
-                        <FontAwesome name="envelope" style={{fontSize:20,color: '#05063F' }} />
-                     <View style={{width: 10}}/> 
-                    <Text style={styles.nfo}>almuntada.ac@gmail.com</Text>
-                        </View>
-                    </View>
-                    <View style={{width: 50}}/> 
-                    <View style={styles.halfReg}>
-                    <View style={{width: 10}}/> 
-                    <View style={{height: 20}}/>
-                    <Image source={require("../../assets/FinalLogo.png")} style={{width: 100, height: 50 }}/>
-                    <View style={{height: 10}}/>
-                    <View style={styles.info}>  
-                  <TouchableOpacity onPress={openInstagram}>
-                    <View>
-                  <FontAwesome name="instagram" style={{fontSize:20,color: '#05063F' }} />
-                 </View>
-                 </TouchableOpacity>
-
-                         
-                        <View style={{width: 15 , }}/>
-                        <TouchableOpacity onPress={openLinkedin}>
-                        <FontAwesome name="linkedin" style={{fontSize:20,color: '#05063F' }} />
-                        </TouchableOpacity>
-                        <View style={{width: 15}}/>
-                        <TouchableOpacity onPress={openFacebook}>
-                        <FontAwesome name="facebook" style={{fontSize:20 ,color: '#05063F'}} />
-                        </TouchableOpacity>
-                        
-                        </View>
-                        </View>
-                        </View>
-                    <View style={{height: 140}}/>
-                   <View style={styles.end} >
-                   <View style={styles.halfReg}>
-                    <View style={{height: 10}}/>
-                   <Text style={styles.TextEnd}>Copyrights.Al-Hassouna-2023 All rights Reserved</Text>
-                   </View>
-                    <View style={styles.halfReg}>
-                    <View style={{height: 10}}/>
-                    <TouchableOpacity onPress={handleOpenPDF}>
-                    <Text style={styles.TextEnd}>Privacy Policy&Terms of use</Text>
-                    </TouchableOpacity>
-                    <View style={{height: 30}}/>
-                    </View>
-                   </View>
+                <End style={styles.End} navigation={navigation} /> 
                     
                    
             </ScrollView>
